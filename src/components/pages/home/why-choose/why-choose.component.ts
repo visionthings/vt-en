501360655c68a492d8991ed56e8a1ce0b6ceb79d
Ahmed Ismael <dev.ahmedismael@outlook.com>
2023-12-11 19:56:51 +0200

    Updated payment to live

--- src/components/pages/home/why-choose/why-choose.component.ts
+++ src/components/pages/home/why-choose/why-choose.component.ts
@@ -17,31 +17,31 @@ export class WhyChooseComponent {
   }[] = [
     {
       id: 1,
-      icon: '../../../../assets/images/why-choose/1.png',
+      icon: 'assets/images/why-choose/1.png',
       title: $localize`السعر أفضل من سعر السوق`,
       description: $localize`تقدم رؤية الأشياء دائما أفضل الأسعار مقارنة بالسوق المحلي والعالمي`,
     },
     {
       id: 2,
-      icon: '../../../../assets/images/why-choose/2.png',
+      icon: 'assets/images/why-choose/2.png',
       title: $localize`تقديم الدعم الفني للمعدات التقنية`,
       description: $localize`كما نقدم الدعم الفني والمساعدة والزيارات لحل المشاكل`,
     },
     {
       id: 3,
-      icon: '../../../../assets/images/why-choose/3.png',
+      icon: 'assets/images/why-choose/3.png',
       title: $localize`توريد المعدات التقنية الجديدة`,
       description: $localize`توفر شركة رؤية الأشياء حلولا تقنية جديدة تماما وأفضل دائما`,
     },
     {
       id: 4,
-      icon: '../../../../assets/images/why-choose/4.png',
+      icon: 'assets/images/why-choose/4.png',
       title: $localize`تقديم الحلول التقنية المناسبة`,
       description: $localize`نحن دائما نختار ونوصي بالاختيار الأفضل والحلول المناسبة لعملائنا`,
     },
     {
       id: 5,
-      icon: '../../../../assets/images/why-choose/5.png',
+      icon: 'assets/images/why-choose/5.png',
       title: $localize`خبرة الشركة في المجال التقني`,
       description: $localize`تتمتع الشركة بالخبرة الكافية في مجالات تكنولوجيا المعلومات على أيدي مهندسينا`,
     },
