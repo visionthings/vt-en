7bc087e8919f0d36636ef9960cd4aa6a689a4620
Ahmed Ismael <dev.ahmedismael@outlook.com>
2023-12-11 19:59:07 +0200

    Updated images in our services

--- src/components/pages/home/our-services/our-services.component.ts
+++ src/components/pages/home/our-services/our-services.component.ts
@@ -13,19 +13,19 @@ export class OurServicesComponent {
   services: { id: number; icon: string; title: string; content: string }[] = [
     {
       id: 1,
-      icon: '../../../../assets/images/our_services/service_1.png',
+      icon: 'assets/images/our_services/service_1.png',
       title: $localize`الاستشارة`,
       content: $localize`نحن نقدم استشارات مجانية وتقييما لاحتياجاتك الأمنية ونوصي بالمنتجات والأنظمة والخدمات الأمنية الأكثر ملائمة لممتلكاتك وميزانيتك`,
     },
     {
       id: 2,
-      icon: '../../../../assets/images/our_services/service_2.png',
+      icon: 'assets/images/our_services/service_2.png',
       title: $localize`التركيب`,
       content: $localize`نقوم بتثبيت الكاميرات والأنظمة الأمنية الخاصة بك باحترافية وكفاءة, مما يضمن وضعها بشكل صحيح وتوصيلها وتهيئتها لتوفير الأداء والتغطية الأمثل`,
     },
     {
       id: 3,
-      icon: '../../../../assets/images/our_services/service_3.png',
+      icon: 'assets/images/our_services/service_3.png',
       title: $localize`التعاقد عن بعد`,
       content: $localize`لأن وقتك يهمنا قمنا بتوفير خدمات التعاقد عن بعد والتي تمكنك من التعاقد وتجديد العقد وطلب زيارة التقني من خلال موقعنا`,
     },
