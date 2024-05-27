e7ae65b8203188b6d6ec398c08ec6a751cd62c06
Ahmed Ismael <dev.ahmedismael@outlook.com>
2024-05-27 03:50:50 +0300

    Added reCAPTCHA

--- src/components/pages/home/header/header.component.ts
+++ src/components/pages/home/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { GreenButtonComponent } from '../../../custom/green-button/green-button.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, GreenButtonComponent],
+  imports: [CommonModule, NgOptimizedImage, GreenButtonComponent, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
